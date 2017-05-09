12c8dbe7bcc4bfbaa5161434ca96c8a8a0595999
Will Kwon <will@kwon.co>
2017-05-08 19:56:24 -0700

    Register defaults

--- Class Directory/AppDelegate.swift
+++ Class Directory/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let preferencesFile = Bundle.main.url(forResource: "Preferences", withExtension: "plist")!
+        let defaultPreferences = NSDictionary.init(contentsOf: preferencesFile)
+        UserDefaults.standard.register(defaults: defaultPreferences as! [String : Any])
+        
         return true
     }
 
